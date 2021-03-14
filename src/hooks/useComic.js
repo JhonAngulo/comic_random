59c88f84a1d728155fbe38220b86cbb6695709e7
Jhon Angulo <jhonmanuelm89@gmail.com>
2021-03-14 17:52:46 -0500

    add linter

--- src/hooks/useComic.js
+++ src/hooks/useComic.js
@@ -1,35 +1,32 @@
-import { useState, useEffect } from "react";
+import { useState, useEffect } from 'react'
 
-const getRequestByProxy = (url) =>{
+const getRequestByProxy = (url) => {
   const proxyUrl = 'https://cors-anywhere.herokuapp.com'
 
   return new Promise((resolve, reject) => {
     fetch(`${proxyUrl}/${url}`, {
       headers: {
-        'X-Requested-With': 'wololo',
-      },
+        'X-Requested-With': 'wololo'
+      }
     })
-    .then(resp => resp.json())
-    .then(res => resolve(res))
-    .catch(error => reject(`Request fail`, error.message))
+      .then(resp => resp.json())
+      .then(res => resolve(res))
+      .catch(error => reject(error))
   })
 }
 
 const useGetComic = () => {
   const urlBase = 'https://xkcd.com/'
-  const urlComplement = 'info.0.json'
-  const [comit, setComit] = useState({});
+  const urlComplement = '/info.0.json'
+  const [comit, setComit] = useState({})
 
   useEffect(async () => {
+    const { num } = await getRequestByProxy(`${urlBase}${urlComplement}`)
+    const comic = await getRequestByProxy(`${urlBase}${num}${urlComplement}`)
+    setComit(comic)
+  }, [])
 
-    const {num} = await getRequestByProxy(`${urlBase}${urlComplement}`)
-    console.log(num)
-    setComit({
-      title: 'one'
-    })
-  }, []);
+  return comit
+}
 
-  return comit;
-};
-
-export default useGetComic;
+export default useGetComic
